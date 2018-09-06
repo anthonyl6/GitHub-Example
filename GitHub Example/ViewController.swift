e8d0a5a731c83bbdf77bf99839eb43d320bd214e
Anthony Loria <anthonyl6@109c02vx09vj1gc.local>
2018-09-06 14:07:13 -0600

    Initial Commit

---
+++ GitHub Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHub Example
+//
+//  Created by Anthony Loria on 2018-09-06.
+//  Copyright © 2018 Anthony Loria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
